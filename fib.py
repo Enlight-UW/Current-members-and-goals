23857c8454c5600086b51bfe72f5493676daed59
Tom Schutte <tjschutte@users.noreply.github.com>
2016-10-04 18:50:04 -0500

    Create fib.py
    Adding in the solution for the interview question for this week.

---
+++ fib.py
@@ -0,0 +1,18 @@
+# An itterative approach
+def fibSeq(n):
+	if (n < 2):
+		return 1
+	prev1 = 1
+	prev2 = 1
+	for i in range(1, n):
+		temp = prev2
+		prev2 = prev1 + prev2
+		prev1 = temp
+	return prev2
+  
+  
+  # A REcursive approach
+  def fib(n):
+	if (n < 2):
+		return 1
+	return fib(n - 1) + fib(n - 2)
